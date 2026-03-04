d4c255349487d74e15739c22b40b6f6524a5c782
AMENAYO <AMENAYO@proton.me>
2026-03-04 13:30:04 +0100

    Create combat.js

---
+++ combat.js
@@ -0,0 +1,40 @@
+let playerLevel = 1;
+let xp = 0;
+
+function attack(scene, player) {
+
+  const projectile = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshStandardMaterial({color: 0xff9900})
+  );
+
+  projectile.position.copy(player.position);
+  scene.add(projectile);
+
+  let speed = 0.5;
+
+  function move() {
+    projectile.position.z -= speed;
+
+    if (projectile.position.z < -200) {
+      scene.remove(projectile);
+      return;
+    }
+
+    requestAnimationFrame(move);
+  }
+
+  move();
+}
+
+function gainXP(amount) {
+  xp += amount;
+
+  if (xp >= playerLevel * 50) {
+    xp = 0;
+    playerLevel++;
+    alert("LEVEL UP ! Niveau " + playerLevel);
+  }
+
+  localStorage.setItem("toblocks_level", playerLevel);
+}
